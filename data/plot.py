173373749323accaa34e8c3e6ecd159222c631ae
Alberto Sonnino <alberto@sonnino.com>
2024-10-06 13:36:13 +0300

    feat: pass on evaluation

--- data/plot.py
+++ data/plot.py
@@ -23,293 +23,6 @@ matplotlib.rcParams["ps.fonttype"] = 42
 # A simple python script to plot measurements results. This script requires
 # the following dependencies: `pip install matplotlib`.
 
-# # -- Parse base line data --
-
-
-# class Setup:
-#     def __init__(self, faults, nodes, workers, collocate, rate, tx_size):
-#         self.nodes = nodes
-#         self.workers = workers
-#         self.collocate = collocate
-#         self.rate = rate
-#         self.tx_size = tx_size
-#         self.faults = faults
-#         self.max_latency = "any"
-
-#     def __str__(self):
-#         return (
-#             f" Faults: {self.faults}\n"
-#             f" Committee size: {self.nodes}\n"
-#             f" Workers per node: {self.workers}\n"
-#             f" Collocate primary and workers: {self.collocate}\n"
-#             f" Input rate: {self.rate} tx/s\n"
-#             f" Transaction size: {self.tx_size} B\n"
-#             f" Max latency: {self.max_latency} ms\n"
-#         )
-
-#     def __eq__(self, other):
-#         return isinstance(other, Setup) and str(self) == str(other)
-
-#     def __hash__(self):
-#         return hash(str(self))
-
-#     @classmethod
-#     def from_str(cls, raw):
-#         faults = int(search(r"Faults: (\d+)", raw).group(1))
-#         nodes = int(search(r"Committee size: (\d+)", raw).group(1))
-
-#         tmp = search(r"Worker\(s\) per node: (\d+)", raw)
-#         if tmp is None:
-#             tmp = search(r"Shard\(s\) per node: (\d+)", raw)
-#         workers = int(tmp.group(1)) if tmp is not None else 1
-
-#         tmp = search(r"Collocate primary and workers: (True|False)", raw)
-#         if tmp is not None:
-#             collocate = "True" == tmp.group(1)
-#         else:
-#             collocate = "True"
-
-#         rate = int(search(r"Input rate: (\d+)", raw).group(1))
-#         tx_size_search = search(r"Transaction size: (\d+)", raw)
-#         if tx_size_search is not None:
-#             tx_size = int(tx_size_search.group(1))
-#         else:
-#             tx_size = 0
-#         return cls(faults, nodes, workers, collocate, rate, tx_size)
-
-
-# class Result:
-#     def __init__(self, mean_tps, mean_latency, std_tps=0, std_latency=0):
-#         self.mean_tps = mean_tps
-#         self.mean_latency = mean_latency
-#         self.std_tps = std_tps
-#         self.std_latency = std_latency
-
-#     def __str__(self):
-#         return (
-#             f" TPS: {self.mean_tps} +/- {self.std_tps} tx/s\n"
-#             f" Latency: {self.mean_latency} +/- {self.std_latency} ms\n"
-#         )
-
-#     @classmethod
-#     def from_str(cls, raw):
-#         tps = int(search(r".* End-to-end TPS: (\d+)", raw).group(1))
-#         latency = int(search(r".* End-to-end latency: (\d+)", raw).group(1))
-#         return cls(tps, latency)
-
-#     @classmethod
-#     def aggregate(cls, results):
-#         if len(results) == 1:
-#             return results[0]
-
-#         mean_tps = round(mean([x.mean_tps for x in results]))
-#         mean_latency = round(mean([x.mean_latency for x in results]))
-#         std_tps = round(stdev([x.mean_tps for x in results]))
-#         std_latency = round(stdev([x.mean_latency for x in results]))
-#         return cls(mean_tps, mean_latency, std_tps, std_latency)
-
-
-# class LogAggregator:
-#     def __init__(self, system, files, max_latencies):
-#         assert isinstance(system, str)
-#         assert isinstance(files, list)
-#         assert all(isinstance(x, str) for x in files)
-#         assert isinstance(max_latencies, list)
-#         assert all(isinstance(x, int) for x in max_latencies)
-
-#         self.system = system
-#         self.max_latencies = max_latencies
-
-#         data = ""
-#         for filename in files:
-#             with open(filename, "r") as f:
-#                 data += f.read()
-
-#         records = defaultdict(list)
-#         for chunk in data.replace(",", "").split("SUMMARY")[1:]:
-#             if chunk:
-#                 records[Setup.from_str(chunk)] += [Result.from_str(chunk)]
-
-#         self.records = {k: Result.aggregate(v) for k, v in records.items()}
-
-#     def print(self):
-#         results = [
-#             self._print_latency(),
-#             self._print_tps(scalability=False),
-#             self._print_tps(scalability=True),
-#         ]
-#         for graph_type, records in results:
-#             for setup, values in records.items():
-#                 data = "\n".join(f" Variable value: X={x}\n{y}" for x, y in values)
-#                 string = (
-#                     "\n"
-#                     "-----------------------------------------\n"
-#                     " RESULTS:\n"
-#                     "-----------------------------------------\n"
-#                     f"{setup}"
-#                     "\n"
-#                     f"{data}"
-#                     "-----------------------------------------\n"
-#                 )
-
-#                 filename = (
-#                     f"{self.system}."
-#                     f"{graph_type}-"
-#                     f"{setup.faults}-"
-#                     f"{setup.nodes}-"
-#                     f"{setup.workers}-"
-#                     f"{setup.collocate}-"
-#                     f"{setup.rate}-"
-#                     f"{setup.tx_size}-"
-#                     f"{setup.max_latency}.txt"
-#                 )
-#                 with open(filename, "w") as f:
-#                     f.write(string)
-
-#     def _print_latency(self):
-#         records = deepcopy(self.records)
-#         organized = defaultdict(list)
-#         for setup, result in records.items():
-#             rate = setup.rate
-#             setup.rate = "any"
-#             organized[setup] += [(result.mean_tps, result, rate)]
-
-#         for setup, results in list(organized.items()):
-#             results.sort(key=lambda x: x[2])
-#             organized[setup] = [(x, y) for x, y, _ in results]
-
-#         return "latency", organized
-
-#     def _print_tps(self, scalability):
-#         records = deepcopy(self.records)
-#         organized = defaultdict(list)
-#         for max_latency in self.max_latencies:
-#             for setup, result in records.items():
-#                 setup = deepcopy(setup)
-#                 if result.mean_latency <= max_latency:
-#                     setup.rate = "any"
-#                     setup.max_latency = max_latency
-#                     if scalability:
-#                         variable = setup.workers
-#                         setup.workers = "x"
-#                     else:
-#                         variable = setup.nodes
-#                         setup.nodes = "x"
-
-#                     new_point = all(variable != x[0] for x in organized[setup])
-#                     highest_tps = False
-#                     for v, r in organized[setup]:
-#                         if result.mean_tps > r.mean_tps and variable == v:
-#                             organized[setup].remove((v, r))
-#                             highest_tps = True
-#                     if new_point or highest_tps:
-#                         organized[setup] += [(variable, result)]
-
-#         [v.sort(key=lambda x: x[0]) for v in organized.values()]
-#         return "tps", organized
-
-
-# class BaselineData:
-#     def __init__(self, data_directory, nodes, faults, transaction_size, system):
-#         self.data_directory = data_directory
-#         self.nodes = nodes
-#         self.faults = faults
-#         self.transaction_size = transaction_size
-#         self.system = system
-
-#     @staticmethod
-#     def cleanup():
-#         [os.remove(x) for x in glob(f"*latency*.txt")]
-
-#     def _natural_keys(self, text):
-#         def try_cast(text):
-#             return int(text) if text.isdigit() else text
-
-#         return [try_cast(c) for c in split("(\d+)", text)]
-
-#     def _tps(self, data):
-#         values = findall(r" TPS: (\d+) \+/- (\d+)", data)
-#         values = [(int(x), int(y)) for x, y in values]
-#         return list(zip(*values))
-
-#     def _latency(self, data):
-#         values = findall(r" Latency: (\d+) \+/- (\d+)", data)
-#         values = [(float(x) / 1000, float(y) / 1000) for x, y in values]
-#         return list(zip(*values))
-
-#     def _variable(self, data):
-#         return [int(x) for x in findall(r"Variable value: X=(\d+)", data)]
-
-#     def for_latency_throughput_plot(self):
-#         [os.remove(x) for x in glob(f"{self.system}.*.txt")]
-#         files = glob(os.path.join(self.data_directory, "*.txt"))
-#         LogAggregator(self.system, files, []).print()
-
-#         x = 10 if self.system.casefold() == "zef".casefold() else 1
-#         filename = (
-#             f"{self.system}."
-#             "latency-"
-#             f"{self.faults}-"
-#             f"{self.nodes}-"
-#             f"{x}-"  # workers / shards
-#             "True-"  # collocate
-#             "any-"
-#             f"{self.transaction_size}-"
-#             "any.txt"
-#         )
-#         if not os.path.isfile(filename):
-#             return None
-
-#         with open(filename, "r") as file:
-#             result = file.read().replace(",", "")
-
-#         x_values = self._variable(result)
-#         y_values, y_err = self._latency(result)
-#         if x_values:
-#             id = PlotDataId(self.system, self.nodes, self.faults)
-#             return PlotData(id, x_values, y_values, y_err)
-
-#     # def plot_tps(self, system, faults, nodes, workers, tx_size, max_latencies):
-#     #     assert isinstance(system, str)
-#     #     assert isinstance(faults, list)
-#     #     assert all(isinstance(x, int) for x in faults)
-#     #     assert isinstance(max_latencies, list)
-#     #     assert all(isinstance(x, int) for x in max_latencies)
-#     #     assert isinstance(tx_size, int)
-
-#     #     scalability = len(workers) > 1
-#     #     collocate = not scalability
-
-#     #     self.reset_markers()
-#     #     self.reset_linestyles()
-
-#     #     self.results = []
-#     #     for f in faults:
-#     #         for l in max_latencies:
-#     #             filename = (
-#     #                 f"{system}."
-#     #                 f"tps-"
-#     #                 f"{f}-"
-#     #                 f'{"x" if not scalability else nodes[0]}-'
-#     #                 f'{"x" if scalability else workers[0]}-'
-#     #                 f"{collocate}-"
-#     #                 f"any-"
-#     #                 f"{tx_size}-"
-#     #                 f"{l}.txt"
-#     #             )
-#     #             if os.path.isfile(filename):
-#     #                 with open(filename, "r") as file:
-#     #                     self.results += [file.read().replace(",", "")]
-
-#     #     self.system = system
-#     #     z_axis = self._max_latency
-#     #     x_label = "Workers per validator" if scalability else "Committee size"
-#     #     y_label = ["Throughput (tx/s)", "Throughput (MB/s)"]
-#     #     marker = next(self.markers)
-#     #     color = next(self.colors)
-#     #     self._plot(x_label, y_label, self._tps, z_axis, "tps", marker, color)
-
-
 # -- Mysticeti data parser --
 
 
@@ -891,6 +604,7 @@ class Plotter:
         x_max=None,
         figure_size=(6.4, 2.4),
         yscale=None,
+        frameon=True,
     ):
         self.plot_name = plot_name
         self.plot_type = plot_type
@@ -900,10 +614,12 @@ class Plotter:
         self.legend_location = legend_location
         self.legend_anchor = legend_anchor
         self.yscale = yscale
+        self.frameon = frameon
 
         self.colors = cycle(["tab:green", "tab:blue", "tab:orange", "tab:red"])
         self.markers = cycle(["o", "v", "s", "d"])
         self.linestyles = cycle(["solid", "dotted"])
+        self.patterns = cycle(["", "//"])
 
         plt.figure(figsize=figure_size)
 
@@ -922,15 +638,17 @@ class Plotter:
         if plot_type == PlotType.L_GRAPH or plot_type == PlotType.HIST:
             if id.system == "Obelia":
                 f = "" if id.faults == 0 else f" ({id.faults} faulty)"
-                a = "" if id.aux == 0 else f", {id.aux} aux"
-                l = f"{id.nodes} core{a}{f}"
-                return f"{id.system} - {l}"
-            else:
-                f = "" if id.faults == 0 else f" ({id.faults} faulty)"
-                l = f"{id.nodes} nodes{f}"
-                return f"{id.system} - {l}"
-        else:
-            return None
+                # a = "" if id.aux == 0 else f",\n{id.aux} aux"
+                # l = f"{id.nodes} core{a}{f}"
+                # return f"{id.system} - {l}"
+                return f"O-{id.nodes}-{id.aux}{f}"
+            elif id.system == "Mysticeti":
+                # f = "" if id.faults == 0 else f" ({id.faults} faulty)"
+                # l = f"{id.nodes} nodes{f}"
+                # return f"{id.system} - {l}"
+                return f"M-{id.nodes}"
+
+        return None
 
     def _axes_labels(self, plot_type):
         if plot_type == PlotType.L_GRAPH:
@@ -940,7 +658,7 @@ class Plotter:
         elif plot_type == PlotType.TIME_SERIES:
             return ("Time (s)", "Latency (s)")
         elif plot_type == PlotType.HIST:
-            return ("Systems", "Latency (s)")
+            return ("Systems at 50k tx/s", "Latency (s)")
         else:
             assert False
 
@@ -959,45 +677,53 @@ class Plotter:
             color=self.color,
             marker=self.marker,
             capsize=3,
+            linewidth=4,
+            markersize=10,
         )
 
     def bar(self, data):
         if data is None:
             return
 
-        # indices, labels = zip(*data.x_values)
-
         plt.bar(
             data.x_values,
             data.y_values,
             color=self.color,
+            hatch=next(self.patterns),
+            label=self._legend_entry(self.plot_type, data.id),
             capsize=3,
         )
+        # plt.xticks([""] * len(data.x_values))
+        # plt.xticks([])
         # plt.xticks(rotation=45, ha="right")
 
     def finalize(self):
         x_label, y_label = self._axes_labels(self.plot_type)
 
+        legend_properties = {"weight": "bold"}
+        plt.legend(
+            loc=self.legend_location,
+            bbox_to_anchor=self.legend_anchor,
+            ncol=self.legend_columns,
+            frameon=self.frameon,
+            prop=legend_properties,
+        )
+
         if not (self.plot_type == PlotType.HIST):
-            plt.legend(
-                loc=self.legend_location,
-                bbox_to_anchor=self.legend_anchor,
-                ncol=self.legend_columns,
-            )
-        if not (self.plot_type == PlotType.HIST):
-            plt.xlim(xmin=0)
+            # plt.xlim(xmin=0)
+            pass
         plt.ylim(bottom=0)
         if self.plot_type == PlotType.L_GRAPH or self.plot_type == PlotType.HIST:
             plt.ylim(top=self.y_max)
             plt.xlim(right=self.x_max)
-        plt.xlabel(x_label, fontweight="bold")
-        plt.ylabel(y_label, fontweight="bold")
+        plt.xlabel(x_label, fontweight="bold", fontsize=14)
+        plt.ylabel(y_label, fontweight="bold", fontsize=14)
 
         if self.yscale:
             plt.yscale(self.yscale)
 
-        plt.xticks(weight="bold")
-        plt.yticks(weight="bold")
+        plt.xticks(weight="bold", fontsize=14)
+        plt.yticks(weight="bold", fontsize=14)
         plt.grid()
         ax = plt.gca()
         if not (self.plot_type == PlotType.HIST):
@@ -1009,7 +735,7 @@ class Plotter:
         plot_directory = self._make_plot_directory()
         for x in ["pdf", "png"]:
             filename = os.path.join(plot_directory, f"{self.plot_name}.{x}")
-            plt.savefig(filename, bbox_inches="tight")
+            plt.savefig(filename, bbox_inches="tight", dpi=300)
 
 
 def overhead():
@@ -1021,9 +747,10 @@ def overhead():
         y_max=1,
         x_max=55000,
         legend_columns=3,
-        legend_location="lower center",
+        legend_location="upper center",
         legend_anchor=(0.5, 1),
         figure_size=(6.4, 2.4),
+        frameon=True,
     )
 
     plotter.new_system()
@@ -1058,31 +785,34 @@ def histogram():
         PlotType.HIST,
         y_max=1,
         legend_columns=3,
-        legend_location="lower center",
+        legend_location="upper center",
         legend_anchor=(0.5, 1),
         figure_size=(6.4, 2.4),
     )
 
+    # Ugly incremental spacing hack in labels to tell matplotlib that they are
+    # different labels
+
     plotter.new_system()
     data = MysticetiDataNew("mysticeti-c-new", 10, 0, transaction_size, "shared")
-    plotter.bar(data.for_histogram("Mysticeti\n10 nodes", 50000))
+    plotter.bar(data.for_histogram("M", 50000))
 
     data = MysticetiDataNew("mysticeti-c-new", 50, 0, transaction_size, "shared")
-    plotter.bar(data.for_histogram("Mysticeti\n50 nodes", 50000))
+    plotter.bar(data.for_histogram(" M ", 50000))
 
     plotter.new_system()
     data = ObeliaData("obelia", 0, 10, 50, transaction_size, "shared")
-    plotter.bar(data.for_histogram("Obelia\n10 core", 50000))
+    plotter.bar(data.for_histogram("O", 50000))
 
     data = ObeliaData("obelia", 0, 50, 200, transaction_size, "shared")
-    plotter.bar(data.for_histogram("Obelia\n50 core", 50000))
+    plotter.bar(data.for_histogram(" O ", 50000))
 
     plotter.new_system()
     data = ObeliaData("obelia", 40, 10, 50, transaction_size, "shared")
-    plotter.bar(data.for_histogram("Obelia\n10 core\n(faulty)", 50000))
+    plotter.bar(data.for_histogram("  O  ", 50000))
 
     data = ObeliaData("obelia", 190, 50, 200, transaction_size, "shared")
-    plotter.bar(data.for_histogram("Obelia\n50 core\n(faulty)", 50000))
+    plotter.bar(data.for_histogram("   O   ", 50000))
 
     plotter.finalize()
 
